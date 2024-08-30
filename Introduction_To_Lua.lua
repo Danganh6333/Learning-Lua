678ff211a329099b3b70f703ae50caae3788b82c
Danganh6333 <134066905+Danganh6333@users.noreply.github.com>
2024-08-30 18:02:12 +0700

    Intro

---
+++ Introduction_To_Lua.lua
@@ -0,0 +1,9 @@
+--In Lua you can use either single quotes or double quotes 
+--single quote
+print('Hello, World') --print is a built-in function so you don't to define it yourself
+--double  quote 
+print("Hello, World")
+
+--escape character(\)
+print("Hello, \"world\"") --The back slash is used to escape the inner double  quote  indicating that they are part of the string and not the string delimiters
+
